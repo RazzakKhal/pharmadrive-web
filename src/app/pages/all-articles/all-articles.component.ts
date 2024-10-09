5d59d79c7f6a1fb7f532c2c91b0213713e83b902
mathias <mathiasguirao34@outlook.fr>
2024-10-09 15:13:22 +0200

    WIP article card

--- src/app/pages/all-articles/all-articles.component.ts
+++ src/app/pages/all-articles/all-articles.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ArticleCardComponent } from '../../shared/components/article-card/article-card.component';
 
 @Component({
   selector: 'app-all-articles',
   standalone: true,
-  imports: [],
+  imports: [ArticleCardComponent],
   templateUrl: './all-articles.component.html',
   styleUrl: './all-articles.component.css'
 })

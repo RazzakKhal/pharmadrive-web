6e8ea4ac404163db893212820059729b76e10205
mathias <mathiasguirao34@outlook.fr>
2024-10-09 15:13:22 +0200

    WIP article card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,16 @@ import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { OrderFormComponent } from './shared/forms/order-form/order-form.component';
 import { HomeClientComponent } from './pages/home-client/home-client.component';
+import { ArticleCardComponent } from "./shared/components/article-card/article-card.component";
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SignInComponent, SignUpComponent, HeaderComponent, HomeClientComponent],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, SignInComponent, SignUpComponent, HeaderComponent, HomeClientComponent, ArticleCardComponent, ReactiveFormsModule],
+  // templateUrl: './app.component.html',
+  templateUrl: './pages/all-articles/all-articles.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
